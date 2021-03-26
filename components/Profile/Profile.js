79c6fffcfdf74996851d3fc6659976e4f732e7a2
Riko Chair Nugroho <riko.chair@gmail.com>
2021-03-26 14:59:33 +0700

    change cv

--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -18,7 +18,7 @@ const Profile = () => {
                 <p className={`${styles.intoduction} HeadingMd`}>
                     <strong className="secondary-color">Hello,</strong>  my name is <strong className="secondary-color">Riko Chair Nugroho</strong>, I'm bachelor of Computer Science. I've  experience with Javascript using ReactJS framework. I always want to learn new things to achieve goals. I am capable of working well in a team and on my own.
             </p>
-                <a href="https://drive.google.com/file/d/1bzdjm39grag9YoKx6l7O2Kl9sDyBUDp4/view?usp=sharing" className="button" target="_blank">VIEW RESUME</a>
+                <a href="https://drive.google.com/file/d/14qSTwlalm5MJv_lCJ27YrcuMCMx84vKo/view?usp=sharing" className="button" target="_blank">VIEW RESUME</a>
             </div>
         </section>
     )
