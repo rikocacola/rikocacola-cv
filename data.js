532bf77b858d858ea82634d2a30f3187d44b15dd
Riko Chair Nugroho <44569540+rikocacola@users.noreply.github.com>
2022-04-18 09:27:20 +0700

    Update data.js

--- data.js
+++ data.js
@@ -39,27 +39,6 @@ export const skillsData = [
 
 export const projectsData = [
     {
-        title: "Mowto",
-        description: "Mowto is an application that I build for my girlfriend to save our moments",
-        image: "/images/projects/mowto.png",
-        link: "https://rikoconatt.netlify.app/",
-        source: "https://github.com/rikocacola/movie-db",
-        techLists: [
-            {
-                name: "MongoDB"
-            },
-            {
-                name: "ExpressJS"
-            },
-            {
-                name: "ReactJS"
-            },
-            {
-                name: "NodeJS"
-            }
-        ]
-    },
-    {
         title: "Movie DB",
         description: "Movie DB is a web application to search movie and the details about the movie like Actors, Writers, Plot, and etc.",
         image: "/images/projects/moviedb.png",
