060fd390261d84f7d9eb12bdc1d32c8cc5b14730
Riko Chair <riko.chair@gmail.com>
2026-06-23 22:42:58 +0700

    feat: update project

--- data.js
+++ data.js
@@ -1,96 +1,101 @@
 export const skillsData = [
-    {
-        imgURL: "/images/logo/icon-html.png",
-        title: "HTML"
-    },
-    {
-        imgURL: "/images/logo/icon-css.png",
-        title: "CSS"
-    },
-    {
-        imgURL: "/images/logo/icon-javascript.png",
-        title: "Javascript"
-    },
-    {
-        imgURL: "/images/logo/icon-react.png",
-        title: "ReactJS"
-    },
-    {
-        imgURL: "/images/logo/icon-next.png",
-        title: "NextJS"
-    },
-    {
-        imgURL: "/images/logo/icon-jquery.png",
-        title: "jQuery"
-    },
-    {
-        imgURL: "/images/logo/icon-vue.png",
-        title: "VueJS"
-    },
-    {
-        imgURL: "/images/logo/icon-sass.png",
-        title: "Sass"
-    },
-    {
-        imgURL: "/images/logo/icon-git.png",
-        title: "git"
-    }
+  {
+    imgURL: "/images/logo/icon-html.png",
+    title: "HTML",
+  },
+  {
+    imgURL: "/images/logo/icon-css.png",
+    title: "CSS",
+  },
+  {
+    imgURL: "/images/logo/icon-javascript.png",
+    title: "Javascript",
+  },
+  {
+    imgURL: "/images/logo/icon-react.png",
+    title: "ReactJS",
+  },
+  {
+    imgURL: "/images/logo/icon-next.png",
+    title: "NextJS",
+  },
+  {
+    imgURL: "/images/logo/icon-jquery.png",
+    title: "jQuery",
+  },
+  {
+    imgURL: "/images/logo/icon-sass.png",
+    title: "Sass",
+  },
+  {
+    imgURL: "/images/logo/icon-git.png",
+    title: "git",
+  },
 ];
 
 export const projectsData = [
-    {
-        title: "Movie DB",
-        description: "Movie DB is a web application to search movie and the details about the movie like Actors, Writers, Plot, and etc.",
-        image: "/images/projects/moviedb.png",
-        link: "https://rikocacola.github.io/movie-db/",
-        source: "https://github.com/rikocacola/movie-db",
-        techLists: [
-            {
-                name: "React.js"
-            },
-            {
-                name: "OMDB API"
-            }
-        ]
-    },
-    {
-        title: "Aniflix",
-        description: "Aniflix is a web application to find anime based on airing, movie, and etc. You can  find the anime trailer, anime score, etc.",
-        image: "/images/projects/aniflix.png",
-        link: "https://rikocacola.github.io/aniflix/",
-        source: "https://github.com/rikocacola/aniflix",
-        techLists: [
-            {
-                name: "React.js"
-            },
-            {
-                name: "Jikan API"
-            }
-        ]
-    }
+  {
+    title: "Kambing Cup App",
+    description:
+      "Kambing Cup App is a web application for managing and organizing competitions between Muslim Astra Group Employees.",
+    image: "/images/projects/kambingcup.png",
+    link: "http://kc.amaliah.id/",
+    source: "https://github.com/rikocacola/kambing-cup-v2",
+    techLists: [
+      {
+        name: "React Router 7",
+      },
+      {
+        name: "Golang",
+      },
+      {
+        name: "Firebase",
+      },
+    ],
+  },
+  {
+    title: "Jelajah Agi",
+    description:
+      "Jelajah Agi is a web application to find travel destinations based on various criteria. You can find information about places, travel tips, and more.",
+    image: "/images/projects/jelajah-agi.png",
+    link: "http://jelajah.amaliah.id/",
+    source: "https://github.com/rikocacola/jelajah-agi",
+    techLists: [
+      {
+        name: "Next JS",
+      },
+      {
+        name: "Firebase",
+      },
+      {
+        name: "Supabase",
+      },
+    ],
+  },
 ];
 
 export const worksData = [
-    {
-        companyName: "Technology Innovation Labs",
-        title: "Frontend Developer",
-        startDate: "June 2021",
-        endDate: "Mei 2022",
-        responsibility: [
-            "Create and Modify Odoo Module for Sister Company",
-            "Create Website for External Client"
-        ],
-        image: "/images/company/tilabs.jpg"
-    },
-    {
-        companyName: "Vlink Consulting Indonesia",
-        title: "Frontend Developer",
-        placement: "Astra International",
-        startDate: "Mei 2022",
-        endDate: "Present",
-        responsibility: [
-            "Develop Astra International Sales Operation Website"
-        ],
-        image: "/images/company/vlink.jpg"
-    }
-]
+  {
+    companyName: "Technology Innovation Labs",
+    title: "Frontend Developer",
+    startDate: "June 2021",
+    endDate: "Mei 2022",
+    responsibility: [
+      "Create and Modify Odoo Module for Sister Company",
+      "Create Website for External Client",
+    ],
+    image: "/images/company/tilabs.jpg",
+  },
+  {
+    companyName: "Vlink Consulting Indonesia & 360 Teknologi Indonesia",
+    title: "Frontend Developer",
+    placement: "Astra International",
+    startDate: "Mei 2022",
+    endDate: "Present",
+    responsibility: [
+      "Lead Frontend Development Team",
+      "Develop Astra International Sales Operation Web Application",
+    ],
+    image: "/images/company/astra.png",
+  },
+];
